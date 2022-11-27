510f838ecaba99284d7f0dce7a877471ed3b6934
Arina <arinavoda@mail.ru>
2022-11-27 09:57:09 +0300

    DZ_4

---
+++ DZ_4/27/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Задача 27");
+Console.Write("Введите число: ");
+string a = Console.ReadLine();
+if (a.All(char.IsDigit))
+{
+    Console.Write(" Сумма цифр в числе: " + GetSum(a));
+}
+else
+{
+    Console.WriteLine("Вы ввели неверное число");
+}
+ 
+string GetSum(string n)
+{
+    int x = 0;
+    for (int i = 0; i < n.Length; i++)
+    {
+        x += int.Parse(n[i].ToString());
+    }
+    return x.ToString();
+}
---
+++ DZ_4/29/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Задача 29"); 
+int[] array = GetArray();
+for (int i = 0; i < 8; i++)
+{
+    Console.Write(array[i].ToString() + " ");
+}
+ 
+int[] GetArray()
+{
+    int[] array = new int[8];
+    Random rand = new Random();
+    for (int y = 0; y < 8; y++)
+    {
+        array[y] = rand.Next(1, 100);
+    }
+    return array;
+}
+
