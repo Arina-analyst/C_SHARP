07213311ee410a2daf908a312ffa44d1ac47274b
Arina <arinavoda@mail.ru>
2022-12-06 20:15:53 +0300

    DZ_6

---
+++ DZ_6/41/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Задача 41");
+int[] array = new int[0];
+int p = 0;
+try
+{
+     while (1 == 1)
+     {
+         Console.Write("Введите число или любой символ для выхода из программы: ");
+         p += GetRes(ref array, Convert.ToInt32(Console.ReadLine()));
+     }
+}
+catch
+{
+     Console.Write("Вы ввели числа: ");
+     for (int i = 0; i < array.Length; i++)
+         Console.Write(array[i] + " ");
+     Console.WriteLine("Из них больше нуля: " + p.ToString());
+}
+
+int GetRes(ref int[] array, int n)
+{
+     Array.Resize(ref array, array.Length + 1);
+     array[array.Length-1] = n;
+     if (n > 0) return 1;
+     else return 0;
+}
---
+++ DZ_6/43/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Задача 43");
+int[] array = GetArray();
+Console.Write("Исходный массив: ");
+for (int i = 0; i < array.Length; i++)
+{
+     Console.Write(array[i].ToString() + " ");
+}
+Console.WriteLine("");
+Array.Sort(array);
+Console.Write("Отсортированный массив: ");
+for (int i = 0; i < array.Length; i++)
+{
+     Console.Write(array[i].ToString() + " ");
+}
+
+int[] GetArray()
+{
+     Random rand = new Random();
+     int n = rand.Next(6, 100);
+     int[] array = new int[n];
+     for (int y = 0; y < n; y++)
+     {
+         array[y] = rand.Next(0, 101);
+     }
+     return array;
+}
